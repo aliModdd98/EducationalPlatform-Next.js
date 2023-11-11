8d87ddff4efb648202a7b212c6107292d65ba934
Ali Mohammad <engalimohammad90@gmail.com>
2023-11-11 13:22:44 +0200

    first comm

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
-
+module.exports = {
+    assetPrefix: './',
+    basePath: process.env.NODE_ENV === 'production' ? '/' : '',
+  };
 module.exports = nextConfig
